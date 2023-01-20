cc5298de825b436317e84298532cead1f1dd246d
Banana-Juice <liao-cheng.jackson@nicholsschool.org>
2023-01-19 22:52:38 -0500

    y

--- advent_of_code/2022-1.py
+++ advent_of_code/2022-1.py
@@ -1,4 +1,7 @@
 
-with open('input1.txt') as f:
+with open('d1.in') as f:
     cals = f.read().split('\n')
     print(cals)
+
+
+#r'C:\\Users\\jliao\\Code\\games_and_stuff\\advent_of_code\\input1.txt'
