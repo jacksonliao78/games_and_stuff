ee566c82b9f7be2390ae146feb52da1644880f46
Banana-Juice <liao-cheng.jackson@nicholsschool.org>
2023-01-19 23:18:14 -0500

    wow

--- advent_of_code/2022-1pt2.py
+++ advent_of_code/2022-1pt2.py
@@ -1,16 +1,16 @@
 
 with open('/Users/jliao/Code/games_and_stuff/advent_of_code/d1.in') as f:
     cals = f.read().split('\n')
-   
+
 def ans():
+    tots = []
     tot = 0
-    highest = 0
     for cal in cals:
         if cal == '':
-            if tot > highest:
-                highest = tot
+            tots.append(tot)
             tot = 0
         else: tot += int(cal)
-    print(highest)
+    tots = sorted(tots)
+    print(sum(tots[-3:]))
 
 ans()
