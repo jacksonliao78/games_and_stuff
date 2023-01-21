beda29e1d738c7edb4049e9f3be6e14d0a713058
Banana-Juice <liao-cheng.jackson@nicholsschool.org>
2023-01-21 15:43:42 -0500

    1 number changed

---
+++ advent_of_code/2022-6pt2.py
@@ -0,0 +1,23 @@
+
+
+with open('/Users/jliao/Code/games_and_stuff/advent_of_code/d6.in') as f:
+    msg = f.read()
+
+def check(msg):
+    alphabet = [False] * 26
+    for char in msg:
+        if alphabet[ord(char) - 97] == True:
+            return False
+        alphabet[ord(char) - 97] = True
+    return True
+
+def ans():
+    start, end = 0, 14
+    while True:
+        if check(msg[start:end]) == True:
+            print(end)
+            break
+        start += 1
+        end += 1
+
+ans()
