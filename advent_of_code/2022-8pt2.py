93ca1e93f6a4757ac98247410e96356e0a262ca4
Banana-Juice <liao-cheng.jackson@nicholsschool.org>
2023-01-22 16:02:26 -0500

    had to reverse two of the lists

--- advent_of_code/2022-8pt2.py
+++ advent_of_code/2022-8pt2.py
@@ -2,48 +2,56 @@
 with open('/Users/jliao/Code/games_and_stuff/advent_of_code/d8.in') as f:
     treerows = f.read().split('\n')
 
-
-
 def le(trees, row, col):
     tree = int(trees[row][col])
     left = [*(trees[row][:col])]
-    for tr in left:
+    count = 0
+    for tr in left[::-1]:
         if int(tr) >= tree:
-            return False
-    return True
+            return count + 1
+        else: count += 1
+    return count
+    
     
 def ri(trees, row, col):
     tree = int(trees[row][col])
     right = [*(trees[row][col + 1:])]
+    count = 0
     for tr in right:
         if int(tr) >= tree:
-            return False
-    return True
+            return count + 1
+        else: count += 1
+    return count
+    
 
 def to(trees, row, col):
     tree = int(trees[row][col])
     top = [*(''.join([trees[i][col] for i in range(row)]))]
-    for tr in top:
+    count = 0
+    for tr in top[::-1]:
         if int(tr) >= tree:
-            return False
-    return True
+            return count + 1
+        else: count += 1
+    return count
 
 def bo(trees, row, col):
     tree = int(trees[row][col])
     bottom = [*(''.join([trees[i][col] for i in range(row + 1, 99)]))]
+    count = 0
     for tr in bottom:
         if int(tr) >= tree:
-            return False
-    return True
+            return count + 1
+        else: count += 1
+    return count
 
 def ans():
-    count = 0
+    scenic = 0
     t = treerows
-    for i in range(0, 99):
-        for j in range(0, 99):
-            if bo(t, i, j) == True or to(t, i, j) == True or le(t, i, j) == True or ri(t, i, j) == True:
-                count += 1
-    print(count)
+    for i in range(1, 98):
+        for j in range(1, 98):
+            if bo(t, i, j) * to(t, i, j) * le(t, i, j) * ri(t, i, j) > scenic:
+                scenic = bo(t, i, j) * to(t, i, j) * le(t, i, j) * ri(t, i, j)   
+    print(scenic)
 
 
 ans() 
