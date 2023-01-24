9810b1d247ad9a66feea96bda0308c168cb2a625
Banana-Juice <liao-cheng.jackson@nicholsschool.org>
2023-01-24 13:19:49 -0500

    set makes life easier

--- advent_of_code/2022-9.py
+++ advent_of_code/2022-9.py
@@ -2,46 +2,10 @@
 with open('/Users/jliao/Code/games_and_stuff/advent_of_code/d9.in') as f:
     moves = f.read().split('\n')
 
-def visited(cord, unique):
-    for thing in unique:
-        if cord == unique:
-            return True
-    return False
-
 def tailcheck(head, tail):
     if (((tail[0]-head[0])**2)+((tail[1]-head[1])**2))**0.5 >= 2:
         return True
     return False
-
-def switch(head, direction, amt, tail):
-    if direction == 'D':
-        for _ in range(amt):
-            head = (head[0], head[1] - 1)
-            if tailcheck(head, tail) == True:
-                tail = movetail(tail, head)
-
-            
-
-
-    elif direction == 'U':
-        pass
-    elif direction == 'L':
-        pass
-    elif direction == 'R':
-        pass
-
-
-def ans():
-    head = (0, 0)
-    tail = (0, 0)
-    dif = [(0, 0)]
-    for move in moves:
-        move = move.split(" ")
-        head = switch(head, move[0], move[1], tail)
-    print(len(dif))
-
-#if visited(tail, dif) == False:
-     #       dif.append(tail)
 
 def movetail(tail, head):
     if tail[0] == head[0]:
@@ -61,7 +25,47 @@ def movetail(tail, head):
             return (head[0], head[1] - 1)
         else: return (head[0], head[1] + 1)
 
+def switch(head, direction, amt, tail, dif):
+    if direction == 'D':
+        for _ in range(int(amt)):
+            head = (head[0], head[1] - 1)
+            if tailcheck(head, tail) == True:
+                tail = movetail(tail, head)
+                dif.append(tail)
+        return head, tail, dif
+    elif direction == 'U':
+        for _ in range(int(amt)):
+            head = (head[0], head[1] + 1)
+            if tailcheck(head, tail) == True:
+                tail = movetail(tail, head)
+                dif.append(tail)
+        return head, tail, dif
+    elif direction == 'L':
+        for _ in range(int(amt)):
+            head = (head[0] - 1, head[1])
+            if tailcheck(head, tail) == True:
+                tail = movetail(tail, head)
+                dif.append(tail)
+        return head, tail, dif
+    elif direction == 'R':
+        for _ in range(int(amt)):
+            head = (head[0] + 1, head[1])
+            if tailcheck(head, tail) == True:
+                tail = movetail(tail, head)
+                dif.append(tail)
+        return head, tail, dif
 
-samplehead = (4, 3)
-sampletail = (6, 2)
-print(movetail(sampletail, samplehead))
+
+def ans():
+    head = (0, 0)
+    tail = (0, 0)
+    dif = [(0, 0)]
+    for move in moves:
+        move = move.split(" ")
+        head, tail, dif = switch(head, move[0], move[1], tail, dif)
+    print(len(set(dif)))
+
+ans() # -> 6745
+
+
+
