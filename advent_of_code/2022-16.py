16ed6db3e2ab6945d0217949b7acb8f82986af23
Banana-Juice <liao-cheng.jackson@nicholsschool.org>
2023-02-03 16:35:12 -0500

    lol

--- advent_of_code/2022-16.py
+++ advent_of_code/2022-16.py
@@ -1,8 +1,17 @@
 
 with open('/Users/jliao/Code/games_and_stuff/advent_of_code/d16.in') as f:
-    things = [thing for thing in f.readlines()]
-    print(things)
+    things = [thing.replace("Valve", "").replace("has flow rate=", "").
+    replace("; tunnels lead to valves", "").
+    replace("; tunnel leads to valve", "").
+    strip()
+    .split(" ") for thing in f.readlines()]
 
+class Pipe:
+    
+    def __init__(self, stuff):
+        self.val = int(stuff[1])
+        self.name = stuff[0]
+        self.neighbors = stuff[2:]
 
 class Solution:
 
@@ -10,6 +19,17 @@ class Solution:
         self.val = 0
         self.info = info
         self.dict = {}
+        self.time = 0
+        self.flowrate = 0
 
     def prep(self):
+        for inf in self.info:
+            thing = Pipe(inf)
+            self.dict.update({thing.name: (thing.neighbors, thing.val)})
+        
+
+    def sol(self):
         pass
+
+a = Solution(things)
+a.prep()
