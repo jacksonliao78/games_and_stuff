3034fa7ef92473be7df20aec4c567473a6900436
Banana-Juice <liao-cheng.jackson@nicholsschool.org>
2023-02-12 20:15:26 -0500

    fixed transition again (yep it was wrong)

--- website/main.js
+++ website/main.js
@@ -1,37 +1,19 @@
-const prevBtn = document.querySelector('.prev-button');
-const nextBtn = document.querySelector('.next-button');
-const slider = document.querySelector('.slider');
-const sliderImgs = Array.from(slider.children);
-const time = 5000;
-let activeSlide = 0;
+const images = document.querySelectorAll(".slider img");
+let currentImage = 0;
 
-//hide all images except the first one
-sliderImgs.forEach((img, index) => {
-  if (index !== 0) {
-    img.style.display = 'none';
-  }
+images[currentImage].classList.add("active");
+
+const prevButton = document.querySelector(".prev-button");
+const nextButton = document.querySelector(".next-button");
+
+prevButton.addEventListener("click", () => {
+  images[currentImage].classList.remove("active");
+  currentImage = (currentImage - 1 + images.length) % images.length;
+  images[currentImage].classList.add("active");
 });
 
-//transition function
-const transition = (sliderImgs, activeSlide, targetSlide) => {
-  sliderImgs[activeSlide].classList.remove('active');
-  sliderImgs[targetSlide].classList.add('active');
-  sliderImgs[activeSlide].style.opacity = 0;
-  sliderImgs[targetSlide].style.display = 'block';
-  setTimeout(() => {
-    sliderImgs[activeSlide].style.display = 'none';
-    sliderImgs[targetSlide].style.opacity = 1;
-  }, 1000);
-}
-
-//next button click
-nextBtn.addEventListener('click', () => {
-  activeSlide = (activeSlide + 1) % sliderImgs.length;
-  transition(sliderImgs, activeSlide, (activeSlide + 1) % sliderImgs.length);
-});
-
-//previous button click
-prevBtn.addEventListener('click', () => {
-  activeSlide = (activeSlide - 1 + sliderImgs.length) % sliderImgs.length;
-  transition(sliderImgs, activeSlide, (activeSlide - 1 + sliderImgs.length) % sliderImgs.length);
+nextButton.addEventListener("click", () => {
+  images[currentImage].classList.remove("active");
+  currentImage = (currentImage + 1) % images.length;
+  images[currentImage].classList.add("active");
 });
