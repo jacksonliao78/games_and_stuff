3c205177ac5a741494e3a0cfb665dd0131a9599f
Banana-Juice <liao-cheng.jackson@nicholsschool.org>
2023-02-12 20:03:05 -0500

    finally got this stupid slider transition to work

--- website/main.js
+++ website/main.js
@@ -1,25 +1,37 @@
-// Get the form element
-var form = document.querySelector("form");
+const prevBtn = document.querySelector('.prev-button');
+const nextBtn = document.querySelector('.next-button');
+const slider = document.querySelector('.slider');
+const sliderImgs = Array.from(slider.children);
+const time = 5000;
+let activeSlide = 0;
 
-// Listen for submit events on the form
-form.addEventListener("submit", function(event) {
-  // Prevent the form from being submitted
-  event.preventDefault();
-
-  // Get the values of the name, email, and message fields
-  var name = document.querySelector("input[name='name']").value;
-  var email = document.querySelector("input[name='email']").value;
-  var message = document.querySelector("textarea[name='message']").value;
-
-  // Log the values to the console
-  console.log("Name: " + name);
-  console.log("Email: " + email);
-  console.log("Message: " + message);
-
-  // Clear the form fields
-  document.querySelector("input[name='name']").value = "";
-  document.querySelector("input[name='email']").value = "";
-  document.querySelector("textarea[name='message']").value = "";
+//hide all images except the first one
+sliderImgs.forEach((img, index) => {
+  if (index !== 0) {
+    img.style.display = 'none';
+  }
 });
 
+//transition function
+const transition = (sliderImgs, activeSlide, targetSlide) => {
+  sliderImgs[activeSlide].classList.remove('active');
+  sliderImgs[targetSlide].classList.add('active');
+  sliderImgs[activeSlide].style.opacity = 0;
+  sliderImgs[targetSlide].style.display = 'block';
+  setTimeout(() => {
+    sliderImgs[activeSlide].style.display = 'none';
+    sliderImgs[targetSlide].style.opacity = 1;
+  }, 1000);
+}
 
+//next button click
+nextBtn.addEventListener('click', () => {
+  activeSlide = (activeSlide + 1) % sliderImgs.length;
+  transition(sliderImgs, activeSlide, (activeSlide + 1) % sliderImgs.length);
+});
+
+//previous button click
+prevBtn.addEventListener('click', () => {
+  activeSlide = (activeSlide - 1 + sliderImgs.length) % sliderImgs.length;
+  transition(sliderImgs, activeSlide, (activeSlide - 1 + sliderImgs.length) % sliderImgs.length);
+});
